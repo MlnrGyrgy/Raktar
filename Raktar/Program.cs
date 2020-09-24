4275b46cab8be32edbf891b0be3d0d569ead3311
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-09-24 08:29:37 +0200

    Beolvasas raktar metodus elkeszitese

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -9,9 +9,27 @@ namespace Raktar
 {
     class Program
     {
+        static List<Termek> termekek = new List<Termek>();
+        static void BeolvasRaktar()
+        {
+           
+            StreamReader raktar = new StreamReader("raktar.csv");
+            while (!raktar.EndOfStream)
+            {
+                /*
+                 * sor[0]-> Kód
+                 *  sor[1] -> Név
+                 * sor[2] -> Ár
+                 * sor[3] -> Darabszám
+                 */
+                 string[] sor = raktar.ReadLine().Split(';');
+                termekek.Add(new Termek(sor[0], sor[1], int.Parse(sor[2]), int.Parse(sor[3])));
+            }
+            raktar.Close();
+        }
         static void Main(string[] args)
         {
-
+            BeolvasRaktar();
             Console.ReadKey();
         }
     }
